90818cff71ade3669ffe1ebf1182062c38de8642
maite san martin <rubenav82@gmail.com>
2024-11-23 12:20:41 +0100

    Detalle en el navegador de páginas en formato móvil, para poder detectar a nivel visual si el botón siguiente o anterior están activos o inactivos

--- seed/precios.js
+++ seed/precios.js
@@ -1,34 +1,34 @@
 // Un seeder es para alimentar la base de datos de forma masiva. Llamamos a las variables como las columnas de la bbdd.
 const precios = [
     {
-        nombre: '0 - $10,000 USD',
+        nombre: '0 - 10,000 €',
     },
     {
-        nombre: '$10,000 - $30,000 USD',
+        nombre: '10,000 € - 30,000 €',
     },
     {
-        nombre: '$30,000 - $50,000 USD',
+        nombre: '30,000 € - 50,000 €',
     },
     {
-        nombre: '$50,000 - $75,000 USD',
+        nombre: '50,000 € - 75,000 €',
     },
     {
-        nombre: '$75,000 - $100,000 USD',
+        nombre: '75,000 € - 100,000 €',
     },
     {
-        nombre: '$100,000 - $150,000 USD',
+        nombre: '100,000 € - 150,000 €',
     },
     {
-        nombre: '$150,000 - $200,000 USD',
+        nombre: '150,000 € - 200,000 €',
     },
     {
-        nombre: '$200,000 - $300,000 USD',
+        nombre: '200,000 € - 300,000 €',
     },
     {
-        nombre: '$300,000 - $500,000 USD',
+        nombre: '300,000 € - 500,000 €',
     },
     {
-        nombre: '+ $500,000 USD',
+        nombre: '+ 500,000 €',
     }
 ]
 
